409f09ae9ceeaec6fcb265be28b7f5ea08be72ae
Nurtugan Nuraly <nurtugan757@gmail.com>
2021-06-13 15:50:47 +0600

    Initial Commit

---
+++ TravelDiscoveryLBTA/TravelDiscoveryLBTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryLBTAApp.swift
+//  TravelDiscoveryLBTA
+//
+//  Created by Nurtugan Nuraly on 6/13/21.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryLBTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
