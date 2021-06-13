5d96891321e1dfa6a70eb09b3eb76cbfca433dcc
Nurtugan Nuraly <nurtugan757@gmail.com>
2021-06-13 16:44:54 +0600

    Setup categories view

--- TravelDiscoveryLBTA/ContentView.swift
+++ TravelDiscoveryLBTA/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView {
+                DiscoverCategoriesView()
+            }.navigationTitle("Discover")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiscoverCategoriesView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 8) {
+                ForEach(0..<5, id: \.self) { _ in
+                    VStack(spacing: 4) {
+                        Spacer()
+                            .frame(width: 50, height: 50)
+                            .background(Color.gray)
+                            .cornerRadius(.infinity)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                        Text("Art")
+                            .font(.system(size: 12, weight: .semibold))
+                    }
+                }
+            }.padding(.horizontal)
+        }
     }
 }
