c1030d9b71aef9f785f958984d000659ce3dcfce
Nurtugan Nuraly <nurtugan757@gmail.com>
2021-06-14 20:22:08 +0600

    Feed with data the destinations section

--- TravelDiscoveryLBTA/ContentView.swift
+++ TravelDiscoveryLBTA/ContentView.swift
@@ -58,6 +58,12 @@ struct DiscoverCategoriesView: View {
 }
 
 struct PopularDestinationsView: View {
+    private let destinations: [Destination] = [
+        .init(name: "Paris", country: "France", imageName: "eiffel_tower"),
+        .init(name: "Tokyo", country: "Japan", imageName: "japan"),
+        .init(name: "New York", country: "US", imageName: "new_york")
+    ]
+    
     var body: some View {
         VStack {
             HStack {
@@ -71,13 +77,28 @@ struct PopularDestinationsView: View {
             
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 8) {
-                    ForEach(0..<5, id: \.self) { _ in
-                        Spacer()
-                            .frame(width: 125, height: 150)
-                            .background(Color.gray)
-                            .cornerRadius(5)
-                            .shadow(color: .gray, radius: 4, x: 0, y: 2)
-                            .padding(.bottom)
+                    ForEach(destinations, id: \.self) { destination in
+                        VStack(alignment: .leading, spacing: 0) {
+                            Image(destination.imageName)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 125, height: 125)
+                                .cornerRadius(5)
+                                .padding(.horizontal, 6)
+                                .padding(.vertical, 6)
+                            Text(destination.name)
+                                .font(.system(size: 12, weight: .semibold))
+                                .padding(.horizontal, 12)
+                            Text(destination.country)
+                                .font(.system(size: 12, weight: .semibold))
+                                .padding(.horizontal, 12)
+                                .padding(.bottom, 8)
+                                .foregroundColor(.secondary)
+                        }
+                        .background(Color(white: 0.9, opacity: 1))
+                        .cornerRadius(5)
+                        .shadow(color: .gray, radius: 4, x: 0, y: 2)
+                        .padding(.bottom)
                     }
                 }.padding(.horizontal)
             }
---
+++ TravelDiscoveryLBTA/Models/Destination.swift
@@ -0,0 +1,12 @@
+//
+//  Destination.swift
+//  TravelDiscoveryLBTA
+//
+//  Created by Nurtugan Nuraly on 6/14/21.
+//
+
+import Foundation
+
+struct Destination: Hashable {
+    let name, country, imageName: String
+}
